14aee1a45c48cfe96d49dcddc756eb60a08fd188
tanushka <bmtanushka@gmail.com>
2023-10-04 15:23:08 +0530

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -91,6 +91,7 @@ $routes->post('/courses/sections/lessons/get', 'App/Courses/LessonController::ge
 $routes->post('/courses/sections/lessons/save', 'App/Courses/LessonController::save', ['filter' => 'authguard:courses-w']);
 $routes->post('/courses/sections/lessons/update', 'App/Courses/LessonController::update', ['filter' => 'authguard:courses-w']);
 $routes->post('/courses/sections/lessons/delete', 'App/Courses/LessonController::delete', ['filter' => 'authguard:courses-d']);
+$routes->post('/courses/sections/lessoncompletion/get', 'App/Courses/LessonCompletionController::get', ['filter' => 'authguard:courses-r']);
 
 $routes->post('/courses/instructors/get', 'App/Courses/InstructorController::get', ['filter' => 'authguard:courses_instructor-r']);
 $routes->post('/courses/instructors/get/trainers', 'App/Courses/InstructorController::getTrainersForLink', ['filter' => 'authguard:courses_instructor-w']);
